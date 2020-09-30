cbbbecc9c5de823ce65651d9ee0dcff64f28bdca
Madeleine Thompson <madeleineth@gmail.com>
2020-09-30 01:15:55 -0400

    switch to WebView for definitions
    Linkified TextView is too glitchy.
    
    Also bump to version 3.0.

--- app/src/main/java/net/mdln/englisc/Streams.java
+++ app/src/main/java/net/mdln/englisc/Streams.java
@@ -1,8 +1,11 @@
 package net.mdln.englisc;
 
+import android.app.Activity;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 final class Streams {
     private Streams() {
@@ -21,6 +24,14 @@ final class Streams {
             return os.toByteArray();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    static String readUtf8Resource(Activity activity, int id) {
+        try (InputStream stream = activity.getResources().openRawResource(id)) {
+            return new String(toByteArray(stream), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("can't load raw resource " + id, e);
         }
     }
 }
--- db/test_gen_db.py
+++ db/test_gen_db.py
@@ -16,29 +16,30 @@ class TestGenDb(unittest.TestCase):
         rex = gen_db.compile_linkify_regex(abbrevs, min_len=3)
         s = '<B>a</B> Hwæt! A. b. c'
         h = gen_db.linkify(s, term_nid, rex, current_nid=11, skip=0)
-        eh = ('<B>a</B> <a href="btc://7">Hwæt</a>! '
-              '<a href="btc://10">A. b.</a> c')
+        eh = ('<B>a</B> <a href="https://btc.invalid/7">Hwæt</a>! '
+              '<a href="https://btc.invalid/10">A. b.</a> c')
         self.assertEqual(eh, h)
 
     def test_linkify_skip(self) -> None:
         term_nid = {'aaa': 8}
         rex = gen_db.compile_linkify_regex([], min_len=3)
         h = gen_db.linkify('aaa aaa', term_nid, rex, current_nid=11, skip=1)
-        eh = 'aaa <a href="btc://8">aaa</a>'
+        eh = 'aaa <a href="https://btc.invalid/8">aaa</a>'
         self.assertEqual(eh, h)
 
     def test_linkify_variant(self) -> None:
         term_nid = {'aaa': 8}
         rex = gen_db.compile_linkify_regex([], min_len=3)
         h = gen_db.linkify('aaa v. aaa', term_nid, rex, current_nid=11, skip=8)
-        eh = 'aaa v. <a href="btc://8">aaa</a>'
+        eh = 'aaa v. <a href="https://btc.invalid/8">aaa</a>'
         self.assertEqual(eh, h)
 
     def test_linkify_with_dashes(self) -> None:
         term_nid = {'a': 8, 'aa': 9}
         rex = gen_db.compile_linkify_regex([], min_len=1)
         h = gen_db.linkify('a a-a a- -a', term_nid, rex, current_nid=0, skip=0)
-        eh = '<a href="btc://8">a</a> <a href="btc://9">a-a</a> a- -a'
+        eh = ('<a href="https://btc.invalid/8">a</a> '
+              '<a href="https://btc.invalid/9">a-a</a> a- -a')
         self.assertEqual(eh, h)
 
 
