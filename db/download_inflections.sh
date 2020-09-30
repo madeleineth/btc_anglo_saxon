8b0b3a0c02172a1266d41d672d4944b679c8b03f
Madeleine Thompson <madeleineth@gmail.com>
2020-09-29 23:36:15 -0400

    update deps and use latest inflections artifact (#34)

--- db/download_inflections.sh
+++ db/download_inflections.sh
@@ -1,5 +1,8 @@
-#!/bin/bash -e
-# Don't pass -x. A token is on the command line.
+#!/bin/bash
 
-artifact_url="https://20-210972025-gh.circle-artifacts.com/0/root/project/output.txt.bz2"
-curl --fail --location "${artifact_url}?circle-token=${CIRCLECI_ARTIFACT_TOKEN}" | bzip2 -dc > db/generator-output.txt
+# Don't pass -x. A token is on the command line.
+set -eu -o pipefail
+
+artifact_url="https://35-210972025-gh.circle-artifacts.com/0/output.txt.bz2"
+curl -o db/generator-output.txt.bz2 --fail --location "${artifact_url}?circle-token=${CIRCLECI_ARTIFACT_TOKEN}"
+bzip2 -d db/generator-output.txt.bz2
