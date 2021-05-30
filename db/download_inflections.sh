9ea285541b5e013f86fa4d1df5ebdb63e1ff82f4
Madeleine Thompson <madeleineth@gmail.com>
2021-05-29 19:16:14 -0700

    bump some versions (#67)

--- db/download_inflections.sh
+++ db/download_inflections.sh
@@ -3,6 +3,6 @@
 # Don't pass -x. A token is on the command line.
 set -eu -o pipefail
 
-artifact_url="https://38-210972025-gh.circle-artifacts.com/0/output.txt.bz2"
+artifact_url="https://41-210972025-gh.circle-artifacts.com/0/output.txt.bz2"
 curl -o db/generator-output.txt.bz2 --fail --location "${artifact_url}?circle-token=${CIRCLECI_ARTIFACT_TOKEN}"
 bzip2 -d db/generator-output.txt.bz2
