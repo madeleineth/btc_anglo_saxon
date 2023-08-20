4e8c4f8e3133a82faaeb664782236a5518e3a3fd
Madeleine Thompson <3121668+madeleineth@users.noreply.github.com>
2023-08-19 23:32:48 -0700

    enable github CI of the python side of the code (#87)
    ...and remove unused CircleCI code.

--- db/download_inflections.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eu -o pipefail
-
-artifact_url="https://output.circle-artifacts.com/output/job/f9e941f4-21ff-4875-a09b-5cb211bf7d33/artifacts/0/output.txt.bz2"
-curl -o db/generator-output.txt.bz2 --fail --location "${artifact_url}"
-bzip2 -d db/generator-output.txt.bz2
